b4e4f8e3a102a5a1cc46f2ba0dd293818832374c
Cristian Martins <marsgristian@gmail.com>
2026-01-12 10:54:08 -0300

    Routing Setup for / and /home

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <main>
+      <h1>Karlbruder</h1>
+      <p>Home page</p>
+    </main>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,10 @@
+const Landing = () => {
+  return (
+    <main>
+      <h1>Karlbruder</h1>
+      <p>Landing page</p>
+    </main>
+  );
+};
+
+export default Landing;
