17e9fb8c957da66220686a8f0f0ae949fd67e13c
Cristian Martins <marsgristian@gmail.com>
2026-01-13 15:48:48 -0300

    Save the access token in loca storage

--- frontend/src/config/cognito.tsx
+++ frontend/src/config/cognito.tsx
@@ -1,8 +1,11 @@
+import { WebStorageStateStore } from "oidc-client-ts";
+
 const cognitoAuthConfig = {
   authority: import.meta.env.VITE_COGNITO_AUTHORITY as string,
   client_id: import.meta.env.VITE_COGNITO_CLIENT_ID as string,
   redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI as string,
   response_type: import.meta.env.VITE_COGNITO_RESPONSE_TYPE as string,
   scope: import.meta.env.VITE_COGNITO_SCOPE as string,
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
 };
 export default cognitoAuthConfig;
