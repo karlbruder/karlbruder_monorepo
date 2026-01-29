152f536f9d84d2667a3ca76b17c36417a7110397
Cristian Martins <marsgristian@gmail.com>
2026-01-29 09:00:14 -0300

    Dockerize Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
+import { useEffect } from "react";
 
 
 function App() {
+
+
+  useEffect(() => {
+    fetch(import.meta.env.VITE_API_URL + "/")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("API Health Check:", data);
+      })
+      .catch((error) => {
+        console.error("Error fetching API health:", error);
+      });
+  }, []);
 
   return (
 
@@ -11,6 +24,8 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/home" element={<Home />} />
     </Routes>
+
+    
 
   )
 }
