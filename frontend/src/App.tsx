00d89f7e8aa2e52d663153587496b71b0816a9aa
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2026-06-19 12:42:12 -0300

    fix: Change page router so root redirect to Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
 import Health from "./pages/Health";
+import Home from "./pages/Home";
 
 function App() {
 
   return (
 
     <Routes>
-      <Route path="/" element={<Landing />} />
+      <Route path="/" element={<Home />} />
       <Route path="/health" element={<Health />} />
     </Routes>
 
