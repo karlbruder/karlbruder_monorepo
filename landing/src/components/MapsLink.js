b3655f7c910141200ed1b1251821f5a978d5603b
Vitor Bonelli <bonelli5.vitor@gmail.com>
2026-06-18 21:54:00 -0300

    fix: remove underline from emoji

--- landing/src/components/MapsLink.js
+++ landing/src/components/MapsLink.js
@@ -1,12 +1,15 @@
 const MapsLink = ({ href, label }) => {
   return (
-    <a
-      href={href}
-      style={{color: "white", fontWeight: "bold" }}
-      rel="noreferrer"
-      target="_blank">
-      📍 {label}
-    </a>
+    <span>
+        📍 
+        <a
+            href={href}
+            style={{color: "white", fontWeight: "bold" }}
+            rel="noreferrer"
+            target="_blank">
+            {label}
+        </a>
+    </span>
   )
 }
 
