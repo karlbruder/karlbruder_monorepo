e13a17c4f40ddb533a4f67a9f69ed75ef5d27a7a
Carlos Fernandes <carlos.vinicius.fernandes@gmail.com>
2026-06-19 12:34:36 -0300

    fix: lint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,10 +5,10 @@ import App from './App.tsx'
 
 import { BrowserRouter } from "react-router-dom";
 
-import { AuthProvider } from "react-oidc-context";
+// import { AuthProvider } from "react-oidc-context";
 
 
-import cognitoAuthConfig from './config/cognito.tsx';
+// import cognitoAuthConfig from './config/cognito.tsx';
 
 
 createRoot(document.getElementById('root')!).render(
