0c1e561c7874ad11f8a62bc115990f5c84d8e5e3
Vitor Bonelli <bonelli5.vitor@gmail.com>
2026-07-15 16:47:22 -0300

    feat: add new partner

--- landing/src/components/Partner.js
+++ landing/src/components/Partner.js
@@ -16,29 +16,31 @@ const Partner = () => {
                 </Row>
                 <Row className='justify-content-center gap-5'>
                     <Col sm={1} md={2} className='px-5'> 
-                        <a href="https://linktr.ee/manziniswordmaker" target="_blank"rel="noopener noreferrer follow"  title="Manzini Swordmaker" >
+                        <a href="https://sparringglove.com/" target="_blank"rel="noopener noreferrer follow"  title="Sparring Gloves" >
+                            <img src={process.env.PUBLIC_URL + `/logos/logo_sparring_gloves.png`} alt="Sparring Gloves Logo" width="150" height="150"/>
+                        </a>
+                    </Col>  
 
+                    <Col sm={1} md={2} className='px-5'> 
+                        <a href="https://linktr.ee/manziniswordmaker" target="_blank"rel="noopener noreferrer follow"  title="Manzini Swordmaker" >
                             <img src={process.env.PUBLIC_URL + `/logos/logo_mazini.png`} alt="Manzini Swordmaker Logo" width="150" height="150"/>
                         </a>
                     </Col>
 
                     <Col sm={1} md={2} className='px-5'> 
                         <a href="https://www.instagram.com/cantodoaco/" target="_blank"rel="noopener noreferrer follow"  title="Canto do Aço" >
-
                             <img src={process.env.PUBLIC_URL + `/logos/logo_canto_do_aco.png`} alt="Canto do Aço Logo" width="150" height="150"/>
                         </a>
                     </Col>
 
                     <Col sm={1} md={2} className='px-5'> 
                         <a href="https://linktr.ee/flechebrasil" target="_blank" rel="noopener noreferrer follow"  title="Flèche" >
-
                             <img src={process.env.PUBLIC_URL + `/logos/logo_fleche.jpg`} alt="Fléche Store Logo" width="150" height="150"/>
                         </a>
                     </Col>
 
                     <Col sm={1} md={2} className='px-5'> 
                         <a href="https://www.faitsdarmes.com/en/" target="_blank"rel="noopener noreferrer follow"  title="Faits D'Armes" >
-
                             <img src={process.env.PUBLIC_URL + `/logos/logo_faits_darmes.png`} alt="Faits d'Armes Store Logo" width="200" height="150"/>
                         </a>
                     </Col>
